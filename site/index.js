1ca8d67939ddcd2d17793a8a7ebede8c0d63a053
David Nelles <david.nelles@gmx.de>
2020-07-05 21:36:46 +0200

    feat: Added function to change the output style The function listens to the change event of the output css selection. The function updates the css file for the output during runtime.

--- site/index.js
+++ site/index.js
@@ -3,12 +3,23 @@ var js = import("../pkg/md_collab.js");
 var md = document.getElementById("md_input");
 md.addEventListener("keyup", render_html);
 
+var css_selection = document.getElementById("output_css_selection");
+//css_selection.onchange(console.log(css_selection.value));
+css_selection.addEventListener('change', specify_output_css);
+
 function render_html() {
     let md_text = md.value;
     js.then(js => {
         res = js.render(md_text);
         document.getElementById("html_output").innerHTML = res
     });
+}
+
+function specify_output_css() {
+    let new_css_link = "output-styles/" + css_selection.value
+
+    //remove old link
+    document.getElementById("output_css").setAttribute("href", new_css_link)
 }
 
 render_html();
