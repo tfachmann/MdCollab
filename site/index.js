0996327b370678de9c16e71fba94cd3d325d21e4
julius.sieg@gmx.de <julius.sieg@gmx.de>
2020-07-06 14:42:07 +0200

    feat: Added function for saving contents

--- site/index.js
+++ site/index.js
@@ -10,3 +10,23 @@ function render_html() {
         document.getElementById("html_output").innerHTML = res
     });
 }
+
+
+function save_contents(contents, filename) {
+    var file = new Blob([contents], {type: 'text/plain'});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
